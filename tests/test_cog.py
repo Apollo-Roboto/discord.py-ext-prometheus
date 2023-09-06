d39dce37713a295a56cf96cd3772727e4862077e
Apollo-Roboto <41392253+Apollo-Roboto@users.noreply.github.com>
2023-09-06 19:48:10 -0400

    Updatedin Workflows for Different Versions (#10)
    * updated workflows
    
    * removed async test (forgot why I had that)
    
    * trying 3.7
    
    * settled on 3.8

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,5 +1,4 @@
 import unittest
-import asyncio
 from discord.ext.prometheus import PrometheusCog
 from discord.ext import commands
 from discord import Intents
@@ -9,18 +8,13 @@ class TestCog(unittest.TestCase):
 	Testing the PrometheusCog class
 	"""
 
-	def test_can_be_instanciated(self):
-		event_loop = asyncio.new_event_loop()
-		asyncio.set_event_loop(event_loop)
+	async def test_can_be_instanciated(self):
 
-		async def run_test():
-			bot = commands.Bot(
-				command_prefix='!',
-				intents=Intents.all()
-			)
+		bot = commands.Bot(
+			command_prefix='!',
+			intents=Intents.all()
+		)
 
-			PrometheusCog(bot)
+		print('test')
 
-		coro = asyncio.coroutine(run_test)
-		event_loop.run_until_complete(coro())
-		event_loop.close()
+		PrometheusCog(bot)
